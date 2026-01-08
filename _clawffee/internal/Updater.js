a7791c273384f3ed24aba3a2b73e2c77e0bbdf6a
PurredCoffee <owo@foxgirl.codes>
2026-01-08 20:25:20 +0100

    implement updating API

--- _clawffee/internal/Updater.js
+++ _clawffee/internal/Updater.js
@@ -1,24 +1,22 @@
 const config = require('../../version.json');
+const { spawn } = require('child_process');
 
-/*
-DEPRECATED
-
-
-Promise.all([
-    fetch(config.url).then((value) => value.json())
-]).then(([json, plugin_json]) => {
-    if(json.name && json.name != config.version && plugin_json.name && plugin_json.name != config.version) {
-        console.log(`\u001b[32mImportant!\n\n\nnew version available! \u001b[0m${json.name}\u001b[32m\n\nvisit \u001b[0;1;3;4mhttp://localhost:4444/internal/update/internal\u001b[0;32m to update clawffee!\n\n${json.body}\n\nWIP`);
-        const { sharedServerData, functions } = require('./Server'); 
-        sharedServerData.internal.update = {ver: json.name, body: json.body};
-        functions['/internal/update/internal'] = () => {
-            // TODO: figure out which assets to download and remember to set header Accept to application/octet-stream and X-GitHub-Api-Version: 2022-11-28
+globalThis.clawffeeInternals.launcher.update_info.then((info) => {
+    if(info.info.message) return console.warn('couldnt check for updates', info.info.message);
+    if(!info.info.tag_name || info.info.tag_name === config.version) return;
+    console.log(`\u001b[32mUpdate available for clawffee! \u001b[0m${info.info.tag_name}\n\n\u001b[32mUpdate now at \u001b[0;1;3;4mhttp://localhost:4444/update/internal\u001b[0m\n\n${info.info.body}`);
+    require('./Server').functions['/update/internal'] = async () => {
+        try {
+            const ret = await globalThis.clawffeeInternals.launcher.runUpdate();
+            if(await ret) return console.error(ret);
+            console.log('Please relaunch clawffee...');
+            prompt();
+            process.exit(0);
+        } catch(e) {
+            console.log(e);
         }
-    } else {
-        console.warn('Could not check for updates!')
     }
-}, e => {
-    console.warn(e);
-});
-*/
+})
+
+
 console.log(`\u001b[0m\n Clawffee Version ${config.version} 🐾`);
